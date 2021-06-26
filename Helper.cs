50ce028b790807164f6d204e83a9a148a702ad55
liuchangxu <liucx0816@163.com>
2021-06-26 22:55:30 +0800

    创建项目

---
+++ Helper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    public class Helper
+    {
+        public static Dictionary<int, List<int>> dataSourceList = new Dictionary<int, List<int>>();
+        /// <summary>
+        /// 初始化三行数据  分别是3，5，7
+        /// </summary>
+        public static void Init()
+        {
+            dataSourceList.Clear();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                var dataList = new List<int>();
+                if (i == 1)
+                {
+                    for (int j = 1; j <= 3; j++)
+                    {
+                        dataList.Add(j);
+                    }
+                    dataSourceList.Add(1, dataList);
+                }
+                if (i == 2)
+                {
+                    for (int j = 4; j <= 8; j++)
+                    {
+                        dataList.Add(j);
+                    }
+                    dataSourceList.Add(2, dataList);
+                }
+                if (i == 3)
+                {
+                    for (int j = 9; j <= 15; j++)
+                    {
+                        dataList.Add(j);
+                    }
+                    dataSourceList.Add(3, dataList);
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// 玩家随机取某一行若干个数
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Tuple<bool, int, List<int>, bool> DrawLots(string name)
+        {
+            var dataList = new List<int>();
+            var rowNumber = 0;
+            var ok = false;
+            var win = false;
+            try
+            {
+                if (dataSourceList.Count > 0)
+                {//如果数据源存在数据则执行
+                    var rnd = new Random();
+                    //key值集合
+                    var keyList = new List<int>();
+                    foreach (var key in dataSourceList.Keys)
+                    {
+                        keyList.Add(key);
+                    }
+                    if (keyList.Count > 0)
+                    {
+                        //随机key值索引
+                        var keyIndex = rnd.Next(0, keyList.Count);
+                        if (keyIndex >= 0)
+                        {
+                            var key = keyList[keyIndex];
+                            if (key > 0)
+                            {
+                                //取行号对应的数据总个数
+                                var rowDataList = dataSourceList.GetValueOrDefault(key);
+                                if (rowDataList != null && rowDataList.Count > 0)
+                                {//如果存在数据则执行
+                                 //取行号对应的数据随机总个数
+                                    var dataNumberCount = rnd.Next(1, rowDataList.Count + 1);
+                                    if (dataNumberCount > 0)
+                                    {
+                                        for (int i = 0; i < dataNumberCount; i++)
+                                        {
+                                            dataList.Add(rowDataList[i]);
+                                        }
+                                        for (int j = 0; j < dataList.Count; j++)
+                                        {//循环删除取出的数据
+                                            rowDataList.Remove(dataList[j]);
+                                        }
+                                        if (rowDataList.Count == 0)
+                                        {
+                                            //如果行号对应的数据都取玩了，则删除行号
+                                            dataSourceList.Remove(key);
+                                        }
+                                        rowNumber = key;
+                                        ok = true;
+                                        win = dataSourceList.Count == 0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ok = false;
+                rowNumber = 0;
+                dataList = null;
+                throw (e);
+            }
+            finally
+            {
+
+            }
+            return new Tuple<bool, int, List<int>, bool>(ok, rowNumber, dataList, win);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("数据初始化...");
+            //初始化数据源  三行 3，5，7
+            Helper.Init();
+
+            //打印数据
+            PrintData();
+
+            Console.WriteLine("加载完成" + "\r\n");
+            Console.WriteLine("请输入任意键开始游戏");
+            Console.ReadKey();
+
+            Console.WriteLine("" + "\r\n");
+            var round = 0;
+            var loop = true;
+            do
+            {
+                round++;
+                Console.WriteLine($"======第{round}轮开始======" + "\r\n");
+                for (int i = 1; i <= 2; i++)
+                {
+                    var resultData = Helper.DrawLots("玩家" + i);
+                    var win = ProcessData(resultData, "玩家" + i);
+                    if (win)
+                    {
+                        Console.WriteLine($"玩家{i}输了！");
+                        loop = false;
+                        break;
+                    }
+                    Console.WriteLine("");
+                }
+                if (loop)
+                {
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("剩余数据：" + "\r\n");
+                    PrintData();
+                }
+                Console.WriteLine($"======第{round}轮结束======" + "\r\n");
+                if (loop)
+                {
+                    Console.WriteLine("请输入任意键继续游戏");
+                    Console.ReadKey();
+                    Console.WriteLine("");
+                }
+
+            } while (loop);
+
+            Console.WriteLine("游戏结束！按任意键结束程序");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 打印数据源
+        /// </summary>
+        public static void PrintData()
+        {
+            if (Helper.dataSourceList.Count > 0)
+            {
+                var keyList = new List<int>();
+                foreach (var key in Helper.dataSourceList.Keys)
+                {
+                    keyList.Add(key);
+                }
+                if (keyList.Count > 0)
+                {
+                    for (int i = 0; i < keyList.Count; i++)
+                    {
+                        Console.WriteLine($"第{keyList[i]}行数据：");
+                        var dataList = Helper.dataSourceList[keyList[i]];
+                        if (dataList.Count > 0)
+                        {
+                            for (int j = 0; j < dataList.Count; j++)
+                            {
+                                Console.Write(dataList[j] + "\t");
+                            }
+                            Console.WriteLine("");
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool ProcessData(Tuple<bool, int, List<int>, bool> data, string name)
+        {
+            if (data.Item1)
+            {
+                Console.WriteLine($"{name} 第{data.Item2}行数据：{data.Item3.Count}个");
+                if (data.Item3 != null)
+                {
+                    for (int i = 0; i < data.Item3.Count; i++)
+                    {
+                        Console.Write(data.Item3[i] + "\t");
+                    }
+                    Console.WriteLine("");
+                }
+                return data.Item4;
+            }
+            else
+            {
+                Console.WriteLine($"{name} 第{data.Item2}行数据：异常错误");
+            }
+            return false;
+        }
+
+    }
+}
